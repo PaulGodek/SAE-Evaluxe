94ed3af3c2eba561db137ff6c4c71b3e295023db
sombek <kilyan.sombe@etu.umontpellier.fr>
2024-10-01 15:51:46 +0200

    feat: Crréation de la vue de liste école basique( copie de utilisateur avec modification)

---
+++ src/vue/utilisateur/vueListeEcole.php
@@ -0,0 +1,25 @@
+<form method="get" action="controleurFrontal.php">
+    <input type="hidden" name="action" value="afficherListeEcole"/>
+    <fieldset>
+        <legend>Mon formulaire :</legend>
+        <p class="InputAddOn">
+            <label class="InputAddOn-item" for="nomEcole">Login&#42;</label>
+            <input class="InputAddOn-field" type="text" placeholder="Ex : IUT Montpellier"  id="nomEcole" required>
+        </p>
+
+        <p class="InputAddOn">
+            <input class="InputAddOn-field" type="submit" value="Rechercher" />
+        </p>
+    </fieldset>
+</form>
+<?php
+
+/** @var Ecole[] $ecoles */
+use App\GenerateurAvis\Modele\DataObject\Ecole;
+
+echo "<h2>Liste des utilisateurs</h2><ul>";
+    foreach ($ecoles as $ecole) {
+    $nomHTML = htmlspecialchars($ecole->getNom());
+    $nomURL = rawurlencode($ecole->getNom());
+    echo '<li><p> l\'école <a href="controleurFrontal.php?action=afficherDetail&login=' . $nomURL . '">' . $nomHTML . '</a> (<a href="controleurFrontal.php?action=afficherFormulaireMiseAJour&login=' . $nomURL . '">Modifier ?</a>, <a href="controleurFrontal.php?action=supprimer&login=' . $nomURL . '">Supprimer ?</a>)</p></li>';
+    }
