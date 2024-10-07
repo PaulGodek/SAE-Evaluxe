a8f3813359292ecd3f4be037985fb8c1cd792cae
Hong Hoa Nguyen <hong-hoa.nguyen@etu.umontpellier.fr>
2024-10-07 15:52:55 +0200

    fix some mistakes and make the site more organized

--- src/vue/siteweb/connexion/connexionAdministrateur.php
+++ src/vue/siteweb/connexion/connexionAdministrateur.php
@@ -1,7 +1,7 @@
 <?php include __DIR__ . '/../header.php'; ?>
 
     <main>
-        <link rel="stylesheet" href="css/connect.css">
+        <link rel="stylesheet" href="../ressources/css/connect.css">
         <div class="container">
             <form method="get">
                 <h2>Connexion administrateur</h2>
--- src/vue/siteweb/header.php
+++ src/vue/siteweb/header.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
-    <link rel="stylesheet" href="css/homestyle.css">
-    <link rel="stylesheet" href="css/charte-graphique-UM.css">
-    <link rel="stylesheet" href="css/button.css">
+    <link rel="stylesheet" href="../ressources/css/homestyle.css">
+    <link rel="stylesheet" href="../ressources/css/charte-graphique-UM.css">
+    <link rel="stylesheet" href="../ressources/css/button.css">
 </head>
 <body>
 <header>
