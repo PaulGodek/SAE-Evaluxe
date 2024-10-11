b3e6def19cc4a0669a73c01f52bb34f4f87c87d0
Daniele Dainiute <daniele.dainiute@etu.umontpellier.fr>
2024-10-11 15:32:52 +0200

    bugfix: space added between the name and surname of a student

--- src/vue/etudiant/detailEtudiant.php
+++ src/vue/etudiant/detailEtudiant.php
@@ -3,4 +3,4 @@
 
 use App\GenerateurAvis\Modele\DataObject\Etudiant;
 
-echo "L'étudiant " . htmlspecialchars($etudiant->getNom()) .htmlspecialchars($etudiant->getPrenom()) ." dont la moyenne est  " . htmlspecialchars($etudiant->getMoyenne()) . ".";
+echo "L'étudiant " . htmlspecialchars($etudiant->getNom())." " .htmlspecialchars($etudiant->getPrenom()) ." dont la moyenne est  " . htmlspecialchars($etudiant->getMoyenne()) . ".";
