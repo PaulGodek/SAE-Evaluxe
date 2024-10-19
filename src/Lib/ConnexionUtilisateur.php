155819698efb053a2c80d62382117c87a2dd5e95
Daniele Dainiute <daniele.dainiute@etu.umontpellier.fr>
2024-10-19 15:26:34 +0200

    bugfix: creation of Connexion Utilisateur and Mot de passe files

---
+++ src/Lib/ConnexionUtilisateur.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\GenerateurAvis\Lib;
+
+use App\GenerateurAvis\Modele\HTTP\Session;
+use App\GenerateurAvis\Modele\Repository\UtilisateurRepository;
+
+class ConnexionUtilisateur
+{
+    private static string $cleConnexion = "_utilisateurConnecte";
+
+    public static function connecter(string $loginUtilisateur): void
+    {
+        $session = Session::getInstance();
+        $session->enregistrer(self::$cleConnexion, $loginUtilisateur);
+    }
+
+    public static function estConnecte(): bool
+    {
+        $session = Session::getInstance();
+        return $session->contient(self::$cleConnexion);
+    }
+
+    public static function deconnecter(): void
+    {
+        $session = Session::getInstance();
+        $session->supprimer(self::$cleConnexion);
+    }
+
+    public static function getLoginUtilisateurConnecte(): ?string
+    {
+        $session = Session::getInstance();
+        return $session->lire(self::$cleConnexion) ?? null;
+    }
+
+    public static function estUtilisateur($login): bool
+    {
+        $session = Session::getInstance();
+        $loginUtilisateurConnecte = $session->lire(self::$cleConnexion) ?? null;
+
+        return $loginUtilisateurConnecte === $login;
+    }
+
+    public static function estAdministrateur(): bool
+    {
+        if (!self::estConnecte()) {
+            return false;
+        }
+        $login = self::getLoginUtilisateurConnecte();
+        $utilisateur = (new UtilisateurRepository())->recupererParClePrimaire($login);
+        return $utilisateur !== null && $utilisateur->getEstAdmin();;
+    }
+}
