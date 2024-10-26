715120c5b5bf4a1bd52bece5d1da291757e047cc
Daniele Dainiute <daniele.dainiute@etu.umontpellier.fr>
2024-10-26 18:31:06 +0200

    bugfix: no double main

--- src/vue/siteweb/accueil.php
+++ src/vue/siteweb/accueil.php
@@ -1,9 +1,7 @@
-<main>
-    <div class="container">
-        <div class="HBox">
-            <h1 class="remplaceBaliseLegend">Bienvenue sur Evaluxe !</h1>
-<!--                <img src="logo">-->
+<div class="container">
+    <div class="HBox">
+        <h1 class="remplaceBaliseLegend">Bienvenue sur Evaluxe !</h1>
+        <!--                <img src="logo">-->
     </div>
-    </div>
-</main>
+</div>
 
