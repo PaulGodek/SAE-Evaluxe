46966b66b64e1d03b4d27eb82413174d8c3e60c1
Hong Hoa Nguyen <hong-hoa.nguyen@etu.umontpellier.fr>
2024-10-26 18:20:29 +0200

    correct the files html

--- src/vue/siteweb/connexion/connexionEtudiant.php
+++ src/vue/siteweb/connexion/connexionEtudiant.php
@@ -1,26 +1,24 @@
-<main>
-    <link rel="stylesheet" href="../ressources/css/connect.css">
-    <div class="container">
-        <form method="get" action="controleurFrontal.php">
-            <input type="hidden" name="action" value="connecter">
-            <input type="hidden" name="controleur" value="utilisateur">
-            <h2>Connexion Étudiant</h2>
-            <fieldset>
-                <p class="InputAddOn">
-                    <label class="InputAddOn-item" for="username">Identifiant&#42;</label>
-                    <input class="InputAddOn-field" type="text" name="login" id="username" placeholder="Identifiant"
-                           required>
-                <p class="InputAddOn">
-                    <label class="InputAddOn-item" for="password">Mot de passe &#42;</label>
-                    <input class="InputAddOn-field" type="password" name="password" id="password"
-                           placeholder="•••••••••" required>
-                <p class="InputAddOn">
-                    <input class="InputAddOn-field" type="submit" id="connectButton" value="Connexion"/>
-                </p>
-            </fieldset>
+<link rel="stylesheet" href="../ressources/css/connect.css">
+<div class="container">
+    <form method="get" action="controleurFrontal.php">
+        <input type="hidden" name="action" value="connecter">
+        <input type="hidden" name="controleur" value="utilisateur">
+        <h2>Connexion Étudiant</h2>
+        <fieldset>
+            <p class="InputAddOn">
+                <label class="InputAddOn-item" for="username">Identifiant&#42;</label>
+                <input class="InputAddOn-field" type="text" name="login" id="username" placeholder="Identifiant"
+                       required>
+            <p class="InputAddOn">
+                <label class="InputAddOn-item" for="password">Mot de passe &#42;</label>
+                <input class="InputAddOn-field" type="password" name="password" id="password"
+                       placeholder="•••••••••" required>
+            <p class="InputAddOn">
+                <input class="InputAddOn-field" type="submit" id="connectButton" value="Connexion"/>
+            </p>
+        </fieldset>
 
-        </form>
-    </div>
-</main>
+    </form>
+</div>
 
 
--- src/vue/siteweb/connexion/preference.php
+++ src/vue/siteweb/connexion/preference.php
@@ -1,13 +1,18 @@
-
-<main>
-
-    <div class="container">
-        <h2 id="remplaceBaliseLegend">Choisissez votre compte</h2>
-        <div class="HBox">
-            <a id="adminChoice" class="button" href="/sae3a-base/web/controleurFrontal.php?action=afficherConnexionAdministrateur&controleur=Connexion">Compte Administrateur</a>
-            <a id="etudiantChoice" class="button" href="/sae3a-base/web/controleurFrontal.php?action=afficherConnexionEtudiant&controleur=Connexion">Compte Étudiant</a>
-            <a id="ecoleChoice" class="button" href="/sae3a-base/web/controleurFrontal.php?action=afficherConnexionEcole&controleur=Connexion">Compte École</a>
-        </div>
-        <br>
-        <p>En vous connectant ou en vous inscrivant, vous confirmez avoir lu et accepté les <a href="../ressources/CGU.pdf" target="_blank" class="link">Conditions Générales d'Utilisation</a> de ce site web.</p>
+<div class="container">
+    <h2 id="remplaceBaliseLegend">Choisissez votre compte</h2>
+    <div class="HBox">
+        <a id="adminChoice" class="button"
+           href="/sae3a-base/web/controleurFrontal.php?action=afficherConnexionAdministrateur&controleur=Connexion">Compte
+            Administrateur</a>
+        <a id="etudiantChoice" class="button"
+           href="/sae3a-base/web/controleurFrontal.php?action=afficherConnexionEtudiant&controleur=Connexion">Compte
+            Étudiant</a>
+        <a id="ecoleChoice" class="button"
+           href="/sae3a-base/web/controleurFrontal.php?action=afficherConnexionEcole&controleur=Connexion">Compte
+            École</a>
     </div>
+    <br>
+    <p>En vous connectant ou en vous inscrivant, vous confirmez avoir lu et accepté les <a href="../ressources/CGU.pdf"
+                                                                                           target="_blank" class="link">Conditions
+            Générales d'Utilisation</a> de ce site web.</p>
+</div>
