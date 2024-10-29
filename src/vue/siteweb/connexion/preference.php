b6e417e3d6887f950b6f243dea0199414dae6177
Hong Hoa Nguyen <hong-hoa.nguyen@etu.umontpellier.fr>
2024-10-29 16:06:06 +0100

    add logo when we choose account to connect

--- src/vue/siteweb/connexion/preference.php
+++ src/vue/siteweb/connexion/preference.php
@@ -1,19 +1,47 @@
-<div class="container">
+<div class="container" id="container_Choix">
     <h2 id="remplaceBaliseLegend">Choisissez votre compte</h2>
     <div class="HBox">
         <a id="adminChoice" class="button"
-           href="/sae3a-base/web/controleurFrontal.php?action=afficherConnexionAdministrateur&controleur=Connexion">Compte
-            Administrateur</a>
+           href="/sae3a-base/web/controleurFrontal.php?action=afficherConnexionAdministrateur&controleur=Connexion">
+            <div>
+                <img id="logoAdmin" src="../ressources/images/admin.png" alt="LogoAdmin">
+            </div>
+            <div>
+                Compte Administrateur
+            </div>
+        </a>
+
         <a id="etudiantChoice" class="button"
-           href="/sae3a-base/web/controleurFrontal.php?action=afficherConnexionEtudiant&controleur=Connexion">Compte
-            Étudiant</a>
+           href="/sae3a-base/web/controleurFrontal.php?action=afficherConnexionEtudiant&controleur=Connexion">
+            <div>
+                <img id="logoStudent" src="../ressources/images/student.png" alt="LogoEtudiant">
+            </div>
+            <div>
+                Compte Étudiant
+            </div>
+        </a>
+
         <a id="ecoleChoice" class="button"
-           href="/sae3a-base/web/controleurFrontal.php?action=afficherConnexionEcole&controleur=Connexion">Compte
-            École</a>
+           href="/sae3a-base/web/controleurFrontal.php?action=afficherConnexionEcole&controleur=Connexion">
+            <div>
+                <img id="logoSchool" src="../ressources/images/school.png" alt="LogoEcole">
+            </div>
+            <div>
+                Compte École
+            </div>
+        </a>
+
         <a id="professeurChoice" class="button"
-           href="/sae3a-base/web/controleurFrontal.php?action=afficherConnexionProfesseur&controleur=Connexion">Compte
-            Professeur</a>
+           href="/sae3a-base/web/controleurFrontal.php?action=afficherConnexionProfesseur&controleur=Connexion">
+            <div>
+                <img id="logoTeacher" src="../ressources/images/teacher.png" alt="LogoProfesseur">
+            </div>
+            <div>
+                Compte Professeur
+            </div>
+        </a>
     </div>
+
     <br>
     <p>En vous connectant ou en vous inscrivant, vous confirmez avoir lu et accepté les <a href="../ressources/CGU.pdf"
                                                                                            target="_blank" class="link">Conditions
