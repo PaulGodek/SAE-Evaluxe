55b81db79f4558da92f55d6a6613d69fef26ebf3
godekp <paul.godek@etu.umontpellier.fr>
2024-11-05 22:53:06 +0100

    refactor: un pauvre point en trop

--- src/vue/etudiant/listeEtudiant.php
+++ src/vue/etudiant/listeEtudiant.php
@@ -23,7 +23,7 @@ use App\GenerateurAvis\Modele\Repository\EtudiantRepository;
 
 echo "<h2>Liste des étudiants</h2> 
         
-    <p><a href='controleurFrontal.php?controleur=etudiant&action=afficherListeEtudiantOrdonneParNom'>Trier par nom</a>&emsp; <a href='controleurFrontal.php?controleur=etudiant&action=afficherListeEtudiantOrdonneParPrenom'>Trier par prenom</a>&emsp; <a href='controleurFrontal.php?controleur=etudiant&action=afficherListeEtudiantOrdonneParParcours'>Trier par parcours</a>.</p> 
+    <p><a href='controleurFrontal.php?controleur=etudiant&action=afficherListeEtudiantOrdonneParNom'>Trier par nom</a>&emsp; <a href='controleurFrontal.php?controleur=etudiant&action=afficherListeEtudiantOrdonneParPrenom'>Trier par prenom</a>&emsp; <a href='controleurFrontal.php?controleur=etudiant&action=afficherListeEtudiantOrdonneParParcours'>Trier par parcours</a></p> 
     
 <ul>";
 foreach ($etudiants as $etudiant) {
