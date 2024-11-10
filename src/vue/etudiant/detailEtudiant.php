731a05ec2eaca5d7d75f1fe7bf4fe3c13ae94417
Daniele Dainiute <daniele.dainiute@etu.umontpellier.fr>
2024-11-10 10:20:30 +0100

    bugfix: admin ne peut pas voir code unique

--- src/vue/etudiant/detailEtudiant.php
+++ src/vue/etudiant/detailEtudiant.php
@@ -7,6 +7,7 @@ use App\GenerateurAvis\Modele\Repository\EtudiantRepository;
 
 $idEtudiant = $etudiant->getIdEtudiant();
 $estEcole = ConnexionUtilisateur::estEcole();
+$estEtudiant = ConnexionUtilisateur::estEtudiant();
 if($estEcole){
     $type = "universite";
 } else {
@@ -59,12 +60,16 @@ switch ($type) {
 
         $etudiantInfo = $result['info'];
         $etudiantDetailsPerSemester = $result['details'];
-        $codeUnique = EtudiantRepository::getCodeUniqueEtudiantConnecte();
+        if($estEtudiant){
+            $codeUnique = EtudiantRepository::getCodeUniqueEtudiantConnecte();
+        }
 
         if ($etudiantInfo) {
             echo '<div class="etudiant-details">';
             echo "<h2>Détails de l'étudiant</h2>";
-            echo "<h3>Code unique: {$codeUnique}</h3>";
+            if($estEtudiant){
+                echo "<h3>Code unique: {$codeUnique}</h3>";
+            }
             echo "<p>Nom: {$etudiantInfo['nom']}</p>";
             echo "<p>Prénom: {$etudiantInfo['prenom']}</p>";
             echo "<p>Id étudiant: {$etudiantInfo['etudid']}</p>";
