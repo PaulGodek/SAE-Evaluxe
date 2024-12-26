6951e1936c4cf4f3fa13788c1b5e5992f4612745
godekp <paul.godek@etu.umontpellier.fr>
2024-12-27 00:45:07 +0100

    feature: ajout des exemples de login et mdp pour que la professeur sache quoi utiliser

--- src/vue/siteweb/accueil.php
+++ src/vue/siteweb/accueil.php
@@ -1,6 +1,33 @@
 <div class="container">
-    <div class="HBox">
+    <div class="VBox">
         <h1 class="remplaceBaliseLegend">Bienvenue sur Evaluxe !</h1>
+        <h2>Pour les différents comptes : </h2>
+        <ul>
+            <li>Administrateur :
+            <ul>
+                <li>login : admin1</li>
+                <li>mot de passe : 123 (les mots de passes seront complexifié pour l'application finale)</li>
+            </ul>
+            </li>
+            <li>Etudiant :
+                <ul>
+                    <li>login : nom 100p</li>
+                    <li>mot de passe : 123</li>
+                </ul>
+            </li>
+            <li>Ecole :
+                <ul>
+                    <li>login : eco3</li>
+                    <li>mot de passe : 123</li>
+                </ul>
+            </li>
+            <li>Professeur :
+                <ul>
+                    <li>login : prof1</li>
+                    <li>mot de passe : 123</li>
+                </ul>
+            </li>
+        </ul>
     </div>
 </div>
 
