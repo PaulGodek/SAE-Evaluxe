1983035ddba04403c130f347028bc82a6155c78f
Daniele Dainiute <daniele.dainiute@etu.umontpellier.fr>
2025-01-07 23:47:11 +0200

    bugfix: si l'avis vient d'être créé, la valeur par défaut est toujours réservée. ensuite, nous pouvons changer. le trigger pour changer les numéros dans le tableau des données personnelles est créé

--- src/vue/professeur/formulaireAvisPersonnaliseEtudiant.php
+++ src/vue/professeur/formulaireAvisPersonnaliseEtudiant.php
@@ -14,7 +14,7 @@
 
         <p class="InputAddOn">
             <label class="InputAddOn-item" for="avis_id"></label>
-            <input class="InputAddOn-field" type="text"  name="avis" placeholder="Entrez votre avis ici..." value="<?php
+            <input class="InputAddOn-field" type="text" name="avis" placeholder="Entrez votre avis ici..." value="<?php
             if (!is_null($avis)) {
                 echo $avis['avis'];
             }
@@ -23,17 +23,17 @@
         <p class="InputAddOn">
             <label class="InputAddOn-item" for="ecoleIngenieur_id">École d'ingénieur ou master en informatique:</label>
             <select name="ecoleIngenieur" id="ecoleIngenieur_id">
-                <option value="Tres favorable" <?php if ($avis['ecoleIngenieur'] == 'Tres favorable') echo 'selected'; ?>>Très favorable</option>
-                <option value="Favorable" <?php if ($avis['ecoleIngenieur'] == 'Favorable') echo 'selected'; ?>>Favorable</option>
-                <option value="Reserve" <?php if ($avis['ecoleIngenieur'] == 'Reserve') echo 'selected'; ?>>Réservé</option>
+                <option value="Tres favorable" <?php if (!is_null($avis) && $avis['ecoleIngenieur'] == 'Tres favorable') echo 'selected'; ?>>Très favorable</option>
+                <option value="Favorable" <?php if (!is_null($avis) && $avis['ecoleIngenieur'] == 'Favorable') echo 'selected'; ?>>Favorable</option>
+                <option value="Reserve" <?php if (is_null($avis) || $avis['ecoleIngenieur'] == 'Reserve') echo 'selected'; ?>>Réservé</option>
             </select>
         </p>
         <p class="InputAddOn">
             <label class="InputAddOn-item" for="masterManagement_id">Master en management:</label>
             <select name="masterManagement" id="masterManagement_id">
-                <option value="Tres favorable" <?php if ($avis['masterManagement'] == 'Tres favorable') echo 'selected'; ?>>Très favorable</option>
-                <option value="Favorable" <?php if ($avis['masterManagement'] == 'Favorable') echo 'selected'; ?>>Favorable</option>
-                <option value="Reserve" <?php if ($avis['masterManagement'] == 'Reserve') echo 'selected'; ?>>Réservé</option>
+                <option value="Tres favorable" <?php if (!is_null($avis) && $avis['masterManagement'] == 'Tres favorable') echo 'selected'; ?>>Très favorable</option>
+                <option value="Favorable" <?php if (!is_null($avis) && $avis['masterManagement'] == 'Favorable') echo 'selected'; ?>>Favorable</option>
+                <option value="Reserve" <?php if (is_null($avis) || $avis['masterManagement'] == 'Reserve') echo 'selected'; ?>>Réservé</option>
             </select>
         </p>
         <p class="InputAddOn">
