ab5ceadd435a13e0fdb50c6af13b31c7e75cfa45
godekp <paul.godek@etu.umontpellier.fr>
2025-01-08 23:36:11 +0100

    feature: accès direct aux données du fichier de config

--- src/Configuration/ConfigurationSite.php
+++ src/Configuration/ConfigurationSite.php
@@ -14,10 +14,10 @@ class ConfigurationSite
     public static function determinerPassageNotes(float $note) : string {
         $jsonFile = file_get_contents( __DIR__ . "/configNotes.json");
         $array = json_decode($jsonFile, true);
-        if ($note < $array["barrier1"]) {
+        if ($note < $array[0]["barrier1"]) {
             return "R";
         }
-        if ($note < $array["barrier2"]) {
+        if ($note < $array[0]["barrier2"]) {
             return "F";
         }
         return "TF";
@@ -27,7 +27,7 @@ class ConfigurationSite
         $jsonFile = file_get_contents(__DIR__ . "/configNotes.json");
         $array = json_decode($jsonFile, true);
 
-        $array["barrier1"] = $newBarrier;
+        $array[0]["barrier1"] = $newBarrier;
         $jsonFile = json_encode($array);
         file_put_contents(__DIR__ . "/configNotes.json", $jsonFile);
     }
@@ -36,8 +36,67 @@ class ConfigurationSite
         $jsonFile = file_get_contents(__DIR__ . "/configNotes.json");
         $array = json_decode($jsonFile, true);
 
-        $array["barrier2"] = $newBarrier;
+        $array[0]["barrier2"] = $newBarrier;
         $jsonFile = json_encode($array);
         file_put_contents(__DIR__ . "/configNotes.json", $jsonFile);
+    }
+
+    public static function addAvis(string $avis) : void {
+        $jsonFile = file_get_contents(__DIR__ . "/configNotes.json");
+        $array = json_decode($jsonFile, true);
+
+        $array[1][$avis] = $array[1][$avis] + 1;
+        $jsonFile = json_encode($array);
+        file_put_contents(__DIR__ . "/configNotes.json", $jsonFile);
+    }
+
+    public static function resetAvis() : void {
+        $jsonFile = file_get_contents(__DIR__ . "/configNotes.json");
+        $array = json_decode($jsonFile, true);
+
+        $array[1]["ingenieurR"] = 0;
+        $array[1]["ingenieurF"] = 0;
+        $array[1]["ingenieurTF"] = 0;
+        $array[1]["managementR"] = 0;
+        $array[1]["managementF"] = 0;
+        $array[1]["managementTF"] = 0;
+        $jsonFile = json_encode($array);
+        file_put_contents(__DIR__ . "/configNotes.json", $jsonFile);
+    }
+
+    public static function getIngenieurR() : int {
+        $jsonFile = file_get_contents(__DIR__ . "/configNotes.json");
+        $array = json_decode($jsonFile, true);
+        return $array[1]["ingenieurR"];
+    }
+
+    public static function getIngenieurF() : int {
+        $jsonFile = file_get_contents(__DIR__ . "/configNotes.json");
+        $array = json_decode($jsonFile, true);
+        return $array[1]["ingenieurF"];
+    }
+
+    public static function getIngenieurTF() : int {
+        $jsonFile = file_get_contents(__DIR__ . "/configNotes.json");
+        $array = json_decode($jsonFile, true);
+        return $array[1]["ingenieurTF"];
+    }
+
+    public static function getManagementR() : int {
+        $jsonFile = file_get_contents(__DIR__ . "/configNotes.json");
+        $array = json_decode($jsonFile, true);
+        return $array[1]["managementR"];
+    }
+
+    public static function getManagementF() : int {
+        $jsonFile = file_get_contents(__DIR__ . "/configNotes.json");
+        $array = json_decode($jsonFile, true);
+        return $array[1]["managementF"];
+    }
+
+    public static function getManagementTF() : int {
+        $jsonFile = file_get_contents(__DIR__ . "/configNotes.json");
+        $array = json_decode($jsonFile, true);
+        return $array[1]["managementTF"];
     }
 }
