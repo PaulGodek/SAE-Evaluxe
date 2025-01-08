c00c53ed262c64f7ca910c8d44a4bf40ce3c3b60
godekp <paul.godek@etu.umontpellier.fr>
2025-01-08 22:29:33 +0100

    feature: ajout des fonctions pour contrôler les paramètres de génération d'avis

--- src/Configuration/ConfigurationSite.php
+++ src/Configuration/ConfigurationSite.php
@@ -10,4 +10,34 @@ class ConfigurationSite
     {
         return self::$dureeExpirationSession;
     }
+
+    public static function determinerPassageNotes(float $note) : string {
+        $jsonFile = file_get_contents( __DIR__ . "/configNotes.json");
+        $array = json_decode($jsonFile, true);
+        if ($note < $array["barrier1"]) {
+            return "R";
+        }
+        if ($note < $array["barrier2"]) {
+            return "F";
+        }
+        return "TF";
+    }
+
+    public static function setBarrier1(float $newBarrier) : void {
+        $jsonFile = file_get_contents(__DIR__ . "/configNotes.json");
+        $array = json_decode($jsonFile, true);
+
+        $array["barrier1"] = $newBarrier;
+        $jsonFile = json_encode($array);
+        file_put_contents(__DIR__ . "/configNotes.json", $jsonFile);
+    }
+
+    public static function setBarrier2(int $newBarrier) : void {
+        $jsonFile = file_get_contents(__DIR__ . "/configNotes.json");
+        $array = json_decode($jsonFile, true);
+
+        $array["barrier2"] = $newBarrier;
+        $jsonFile = json_encode($array);
+        file_put_contents(__DIR__ . "/configNotes.json", $jsonFile);
+    }
 }
