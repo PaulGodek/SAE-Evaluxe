3459cffc6a711b6650cd1180f825a5d1d6edde67
godekp <paul.godek@etu.umontpellier.fr>
2025-01-10 08:19:21 +0100

    refactor: affichage de l'accueil

--- src/vue/siteweb/accueil.php
+++ src/vue/siteweb/accueil.php
@@ -1,33 +1,6 @@
 <div class="container">
     <div class="VBox">
         <h1 class="remplaceBaliseLegend">Bienvenue sur Evaluxe !</h1>
-        <h2>Pour les différents comptes : </h2>
-        <ul>
-            <li>Administrateur :
-            <ul>
-                <li>login : admin1</li>
-                <li>mot de passe : 123 (les nouveaux mots de passe respecteront un pattern correct, il s'agit d'ancien mots de passe facile à utiliser qui seront supprimé lors de la sortie finale)</li>
-            </ul>
-            </li>
-            <li>Etudiant :
-                <ul>
-                    <li>login : nom 100p</li>
-                    <li>mot de passe : 123</li>
-                </ul>
-            </li>
-            <li>Ecole :
-                <ul>
-                    <li>login : eco3</li>
-                    <li>mot de passe : 123</li>
-                </ul>
-            </li>
-            <li>Professeur :
-                <ul>
-                    <li>login : prof1</li>
-                    <li>mot de passe : 123</li>
-                </ul>
-            </li>
-        </ul>
     </div>
 </div>
 
